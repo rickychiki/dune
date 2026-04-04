191f5f04e1dd48c8ae35adeeff1e2c669ae47d42
rickychiki <rickychiki@icloud.com>
2026-04-04 22:58:49 +0100

    add

---
+++ data/anime.js
@@ -0,0 +1,302 @@
+window.anime = [
+  {
+    "uri": "spotify:track:2NasEYCMz45B4see4iDqS0",
+    "name": "ドラえもんのうた (OP)",
+    "anime": "ドラえもん",
+    "date": "1979-01-01"
+  },
+  {
+    "uri": "spotify:track:2mMKWdXYz7vVelTbt69UJo",
+    "name": "愛をとりもどせ!!",
+    "anime": "北斗の拳",
+    "date": "1984-01-01"
+  },
+  {
+    "uri": "spotify:track:5CwhBU7eqQgkOVMnRNuzzT",
+    "name": "Get Wild",
+    "anime": "シティーハンター",
+    "date": "1987-01-01"
+  },
+  {
+    "uri": "spotify:track:252lj7Oy8JzIKVECJX4gpi",
+    "name": "ムーンライト伝説",
+    "anime": "美少女戦士セーラームーン",
+    "date": "1992-01-01"
+  },
+  {
+    "uri": "spotify:track:23phSRwoMy48rwFpmuAP8q",
+    "name": "残酷な天使のテーゼ",
+    "anime": "新世紀エヴァンゲリオン",
+    "date": "1995-01-01"
+  },
+  {
+    "uri": "spotify:track:2wN5DYZkj1EHlX2obTNBFj",
+    "name": "檄! 帝国華撃団",
+    "anime": "サクラ大戦",
+    "date": "1996-01-01"
+  },
+  {
+    "uri": "spotify:track:0IL7LGrnYObWYyYaJ3GzWr",
+    "name": "そばかす",
+    "anime": "るろうに剣心 -明治剣客浪漫譚-",
+    "date": "1996-01-01"
+  },
+  {
+    "uri": "spotify:track:2Yv0FavwiRO1mzaIThDAiM",
+    "name": "Bad Apple!!",
+    "anime": "東方幻想郷",
+    "date": "1998-01-01"
+  },
+  {
+    "uri": "spotify:track:4A3G4Chh8DTJftwJEf5tOp",
+    "name": "Butter-Fly （デジモンアドベンチャー全話OP）",
+    "anime": "デジモンアドベンチャー",
+    "date": "1999-01-01"
+  },
+  {
+    "uri": "spotify:track:1PdWDLJNk040dkuXoLLE2F",
+    "name": "ウィーアー!",
+    "anime": "ONE PIECE",
+    "date": "1999-01-01"
+  },
+  {
+    "uri": "spotify:track:17YUGZ488r8GzhbfclLQzq",
+    "name": "GO!!! (Naruto Opening 4)",
+    "anime": "NARUTO -ナルト-",
+    "date": "2002-01-01"
+  },
+  {
+    "uri": "spotify:track:7zty9mmmuqQsn8s4zDH4nk",
+    "name": "メリッサ",
+    "anime": "鋼の錬金術師",
+    "date": "2003-01-01"
+  },
+  {
+    "uri": "spotify:track:1FqOxfA4ghGxSrpNrjGhVx",
+    "name": "瞳の中の迷宮",
+    "anime": "ヤミと帽子と本の旅人",
+    "date": "2004-01-01"
+  },
+  {
+    "uri": "spotify:track:7gaaMd98dW4ssle6fc1fBh",
+    "name": "逆さまの蝶",
+    "anime": "地獄少女",
+    "date": "2005-01-01"
+  },
+  {
+    "uri": "spotify:track:2JacUWw1kxN2lunFlCLrPe",
+    "name": "鳥の詩",
+    "anime": "AIR",
+    "date": "2005-01-01"
+  },
+  {
+    "uri": "spotify:track:4UVQnzCZX2Uw9hq4Ano1Ux",
+    "name": "ハレ晴レユカイ - Ver.涼宮ハルヒ",
+    "anime": "涼宮ハルヒの憂鬱",
+    "date": "2006-01-01"
+  },
+  {
+    "uri": "spotify:track:4YnXlBbFnMJf6qiO2LQMlU",
+    "name": "God knows...",
+    "anime": "涼宮ハルヒの憂鬱",
+    "date": "2006-01-01"
+  },
+  {
+    "uri": "spotify:track:6bPPyigCphBBQ9781j6eKM",
+    "name": "COLORS",
+    "anime": "コードギアス 反逆のルルーシュ",
+    "date": "2006-01-01"
+  },
+  {
+    "uri": "spotify:track:43FWf8KrIofEGueGabxPkK",
+    "name": "旅の途中 (狼と香辛料)",
+    "anime": "狼と香辛料",
+    "date": "2008-01-01"
+  },
+  {
+    "uri": "spotify:track:08hU4ic3BmXofI27o0vNCY",
+    "name": "only my railgun",
+    "anime": "とある科学の超電磁砲",
+    "date": "2009-01-01"
+  },
+  {
+    "uri": "spotify:track:77LQNv3wdUvN5sXG9v5H9u",
+    "name": "Don't Say \"\"Lazy\"\" (From \"\"K-On!\"\")",
+    "anime": "けいおん!",
+    "date": "2009-01-01"
+  },
+  {
+    "uri": "spotify:track:434YBNY61Y9sqBSp7OINBa",
+    "name": "君の知らない物語",
+    "anime": "化物語",
+    "date": "2009-01-01"
+  },
+  {
+    "uri": "spotify:track:4jYQqcYSoYy8iAwB5DloJz",
+    "name": "My Soul Your Beats!",
+    "anime": "Angel Beats!",
+    "date": "2010-01-01"
+  },
+  {
+    "uri": "spotify:track:6SrKLkuqWyKxSxzvtRWvX5",
+    "name": "コネクト",
+    "anime": "魔法少女まどか☆マギカ",
+    "date": "2011-01-01"
+  },
+  {
+    "uri": "spotify:track:7Bh5pZWryHBwxh13y7QTrt",
+    "name": "secret base 〜君がくれたもの〜",
+    "anime": "あの日見た花の名前を僕達はまだ知らない。",
+    "date": "2011-01-01"
+  },
+  {
+    "uri": "spotify:track:3VrY5AUgxJJsmw9HMP4w6W",
+    "name": "Magia",
+    "anime": "魔法少女まどか☆マギカ",
+    "date": "2011-01-01"
+  },
+  {
+    "uri": "spotify:track:4keYzo4xwFG5XgfBlh9SUW",
+    "name": "Hacking To The Gate",
+    "anime": "STEINS;GATE",
+    "date": "2011-01-01"
+  },
+  {
+    "uri": "spotify:track:5UNdjFCgpr2czfhKrT1Tmw",
+    "name": "The Everlasting Guilty Crown",
+    "anime": "ギルティクラウン",
+    "date": "2011-01-01"
+  },
+  {
+    "uri": "spotify:track:38mUTswfszVFQ952r89AP8",
+    "name": "千本桜",
+    "anime": "初音ミク",
+    "date": "2012-01-01"
+  },
+  {
+    "uri": "spotify:track:3mScGCzxiXA9OaHdBeuk7O",
+    "name": "crossing field",
+    "anime": "ソードアート・オンライン",
+    "date": "2012-01-01"
+  },
+  {
+    "uri": "spotify:track:1xAjcHiQRXrrduJeoYRXLG",
+    "name": "Chase the world",
+    "anime": "アクセル・ワールド",
+    "date": "2012-01-01"
+  },
+  {
+    "uri": "spotify:track:7CoXiUY4GvZqnKtM6oWvYT",
+    "name": "デート・ア・ライブ",
+    "anime": "デート・ア・ライブ",
+    "date": "2013-01-01"
+  },
+  {
+    "uri": "spotify:track:4WS4FgOG1fOZl1VuD5aeQX",
+    "name": "回レ!雪月花",
+    "anime": "機巧少女は傷つかない",
+    "date": "2013-01-01"
+  },
+  {
+    "uri": "spotify:track:0F8psXXh2oxE5BGdkKaoIs",
+    "name": "紅蓮の弓矢",
+    "anime": "進撃の巨人",
+    "date": "2013-01-01"
+  },
+  {
+    "uri": "spotify:track:18asIXvfX6RyEtdiGKpTF5",
+    "name": "THIS GAME",
+    "anime": "ノーゲーム・ノーライフ",
+    "date": "2014-01-01"
+  },
+  {
+    "uri": "spotify:track:4lFGExZoEArB7jzulnWtfl",
+    "name": "Daydream café",
+    "anime": "ご注文はうさぎですか？",
+    "date": "2014-01-01"
+  },
+  {
+    "uri": "spotify:track:6uR92injo4PDzSrOPRFfHt",
+    "name": "シュガーソングとビターステップ",
+    "anime": "血界戦線",
+    "date": "2015-01-01"
+  },
+  {
+    "uri": "spotify:track:0WjYHf18yTqGEQjLRHLNWp",
+    "name": "Redo",
+    "anime": "Re:ゼロから始める異世界生活",
+    "date": "2016-01-01"
+  },
+  {
+    "uri": "spotify:track:4AttA0ULabd5N5EBDw2jIO",
+    "name": "前前前世",
+    "anime": "君の名は。",
+    "date": "2016-01-01"
+  },
+  {
+    "uri": "spotify:track:6avH20eR9SGNwV1H0vzHed",
+    "name": "ようこそジャパリパークへ",
+    "anime": "けものフレンズ",
+    "date": "2017-01-01"
+  },
+  {
+    "uri": "spotify:track:3pKGijaLa4b6Fq0zCniPuo",
+    "name": "One Last You (From \"\"Xenoblade Chronicles 2\"\") - Piano/Vocal Cover",
+    "anime": "ゼノブレイド2",
+    "date": "2017-01-01"
+  },
+  {
+    "uri": "spotify:track:1n7h7TRGicLtXDZJcm8IGO",
+    "name": "コレカラ",
+    "anime": "りゅうおうのおしごと！",
+    "date": "2018-01-01"
+  },
+  {
+    "uri": "spotify:track:18dWBsEdWdGA1MRPN95H1X",
+    "name": "FLARE",
+    "anime": "ありふれた職業で世界最強",
+    "date": "2018-01-01"
+  },
+  {
+    "uri": "spotify:track:1hFsEbHxsfflhLEPCq5tzk",
+    "name": "紅蓮華",
+    "anime": "鬼滅の刃",
+    "date": "2019-01-01"
+  },
+  {
+    "uri": "spotify:track:06XQvnJb53SUYmlWIhUXUi",
+    "name": "怪物",
+    "anime": "BEASTARS",
+    "date": "2019-01-01"
+  },
+  {
+    "uri": "spotify:track:3r5h4WbAe3J4UtucnQwcD7",
+    "name": "CHAIN",
+    "anime": "ヴィジュアルプリズン",
+    "date": "2021-01-01"
+  },
+  {
+    "uri": "spotify:track:6iIUjQjRX0Wy6Ri179w9SP",
+    "name": "KEEP WEAVING YOUR SPIDER WAY",
+    "anime": "蜘蛛ですが、なにか？",
+    "date": "2021-01-01"
+  },
+  {
+    "uri": "spotify:track:1ocxu4f7ZOjOcwAbqQ8l1T",
+    "name": "HIGHEST",
+    "anime": "陰の実力者になりたくて！",
+    "date": "2022-01-01"
+  },
+  {
+    "uri": "spotify:track:7xm0KJMfeaJQmQdDxAipiY",
+    "name": "チキチキバンバン",
+    "anime": "パリピ孔明",
+    "date": "2022-01-01"
+  },
+  {
+    "uri": "spotify:track:7ovUcF5uHTBRzUpB6ZOmvt",
+    "name": "アイドル",
+    "anime": "【推しの子】",
+    "date": "2023-01-01"
+  }
+];
